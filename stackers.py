4bfe0f0d0c85366458f99a916b6e94646eebaa26
ruthie1246 <ruthyao308@gmail.com>
2018-07-25 13:20:05 -0500

    pixel moves across bottom row

---
+++ stackers.py
@@ -0,0 +1,35 @@
+from sense_hat import SenseHat
+from pygame.locals import *
+import pygame
+import time
+#this script demonstrates how to create a class structure for gaming mode
+sense = SenseHat()
+sense.clear()
+
+blue = (0, 0, 255)
+
+class stack():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_mode((640, 480))
+        self.gaming = True
+
+    def startGame(self):
+        pygame.time.set_timer(USEREVENT +1, 800)
+        x = 0
+        y = 7
+        while self.gaming:
+            for event in pygame.event.get():
+                sense.set_pixel(x, y, blue)
+                time.sleep(1)
+                sense.set_pixel(x, y, (0,0,0))
+                x += 1
+                if x == 8:
+                    x = 0
+                    
+if __name__ == "__main__":
+    try:
+        game = stack()
+        game.startGame()
+    except KeyboardInterrupt:
+        sense.clear()
